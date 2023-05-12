ee166e332da485bef984ea3795cd1e771bc1c793
nikkster <nikkster311@gmail.com>
2023-05-12 15:23:42 -0700

    create json mock db, add it to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import IndexPage from './components/IndexPage.js';
-import './App.css';
+import IndexPage from "./components/IndexPage.js";
+import { react, useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [stamps, setStamps] = useState([]);
+
+  const getData = () => {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:3030/stamps", requestOptions)
+      .then((response) => response.json())
+      .then((result) => setStamps(result))
+      .catch((error) => console.log("error", error));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-      <IndexPage />
+    <>
+      <IndexPage stamps={stamps} />
+    </>
   );
 }
 
