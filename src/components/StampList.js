d7d2f20ae3231c49b8fbbbd25331fcf80674ebd0
nikkster <nikkster311@gmail.com>
2023-05-30 13:54:34 -0700

    add dynamic list styling, add click on stamp functionality

--- src/components/StampList.js
+++ src/components/StampList.js
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 import Stamp from "./Stamp";
 
-function StampList({ stamps }) {
+function StampList({ stamps, type }) {
+  const defaultIndex = -1;
+  const [selectedStamp, setSelectedStamp] = useState(defaultIndex);
+
+  function CloseStamp() {
+    setSelectedStamp(-1);
+  }
+
   return (
-    <div class="stamp-list-component">
+    <div className="stamp-list-component">
       <h3>StampList Component</h3>
-      <section className="stamp-list-wrapper">
-        <ul class="title-row">
+      {/* close button to close out stamp only shows when a stamp is clicked on and being viewed */}
+      <button
+        className={selectedStamp === -1 ? "close-btn hide" : "close-btn show"}
+        onClick={() => CloseStamp()}
+      >
+        X
+      </button>
+      <section className={`stamp-` + type + `-wrapper`}>
+        <ul key="title-row" className="title-row">
           <li key="checkbox-01"></li>
           <li key="img-02">Image</li>
           <li key="name-03">Name</li>
@@ -14,7 +29,15 @@ function StampList({ stamps }) {
           <li key="dateAquired-06">Date Aquired</li>
         </ul>
         {stamps.map((stamp, index) => (
-          <Stamp key={index} stamp={stamp} />
+          <div
+            key={index}
+            className={selectedStamp === index ? "stamp-active" : ""}
+            onClick={() => {
+              setSelectedStamp(index);
+            }}
+          >
+            <Stamp key={index} stamp={stamp} />
+          </div>
         ))}
       </section>
     </div>
